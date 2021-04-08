f2582c18f349c48e90091c2cbf7a8d4c19d12ed1
Ed <edcsalter@gmail.com>
2021-04-08 02:33:55 -0400

    delegation

---
+++ Design Patterns.playground/Pages/OverView.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+/*:
+# Behavioral
+ ## [Delegation](Delegation)
+* allows one object to use another object to provide data or perform a task on behalf of the first object
+* iOS specific:
+ * ```DataSource``` groups methods that *provide* data
+ * ```Delegate``` groups methods that *receive* data
+*/
